9b96faf8e97f7c98121fceb3c5a0d2286b9597ce
Abhinav Goyal <abhinavgoyal@Abhinavs-MacBook-Pro.local>
2017-10-05 17:26:14 +0800

    lab 3

--- lab3/index.js
+++ lab3/index.js
@@ -1,4 +1,6 @@
 let lastRecord = 0;
+
+// for loading tasks from the db
 function pullMore() {
 	let xmlhttp;
 	if (window.XMLHttpRequest) {
@@ -8,27 +10,34 @@ function pullMore() {
 	}
 	xmlhttp.onreadystatechange = function() {
 		if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			document.getElementById("note").innerHTML = xmlhttp.responseText;
-			xmlhttp.open("GET", `queryNote.php?lastRecord=${lastRecord}`, true);
-			xmlhttp.send();
+			document.getElementById("Note").innerHTML = xmlhttp.responseText;
 		}
-	}
+	};
+	xmlhttp.open("GET", `queryNote.php?lastRecord=${lastRecord}`, true);
+	xmlhttp.send();
 	lastRecord += 3;
-}
-// const span = document.getElementById()
-// function changeState(elem) {
-// 	const oldValue = elem.innerHTML;
-// 	const noteID = elem.parentNode.getAttribute('id');
-// 	const xmlhttp = new XMLHttpRequest();
-// 	if(oldValue==='Y') {
-// 		newValue==='N';
-// 	} else {
-// 		newValue==='Y';
-// 	}
-// 	// ajax code
-// 	xmlhttp.onreadystatechange = function() {
-// 		if(this.readyState === 4 && this.status === 200) {
+};
 
-// 		}
-// 	}
-// }
+// toggle yes - done
+
+
+function changeState(elem) {
+	const oldValue = elem.innerHTML;
+	const noteID = elem.parentNode.getAttribute('id');
+	const xmlhttp = new XMLHttpRequest();
+	let newValue;
+	if(oldValue==='Y') {
+		newValue='N';
+	} else {
+		newValue='Y';
+	}
+	console.log(newValue);
+	// ajax code
+	xmlhttp.onreadystatechange = function() {
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			elem.innerHTML = newValue;
+		}
+	};
+	xmlhttp.open("GET", `updateNoteState.php?id=${noteID}&newValue=${newValue}`);
+	xmlhttp.send();
+};
--- lab3/queryNote.php
+++ lab3/queryNote.php
@@ -1,21 +1,21 @@
 <?php
 
 // connect to the database
-$conn=mysqli_connect('sophia.cs.hku.hk', 'agoyal', 'zIvdQsUN') or die('Error!'.mysqli_error($conn));
+$conn=mysqli_connect('localhost', 'comp3322', 'agoyal') or die('Error!1'.mysqli_error($conn));
 
 // select database
-mysqli_select_db($conn, 'agoyal') or die('Error!'.mysqli_error($conn));
+mysqli_select_db($conn, 'comp3322') or die('Error!2'.mysqli_error($conn));
 
 // defining the sql query
-$query = 'SELECT * FROM checklist LIMIT'.$_GET['lastRecord'].',3';
+$query = 'SELECT * FROM checklist LIMIT '.$_GET['lastRecord'].',3';
 
 // executing sql query
-$result = mysqli_query($conn, $query) or die('Error!'.mysql_error($conn));
+$result = mysqli_query($conn, $query) or die('Error!3'.mysqli_error($conn));
 
 // display data from the database
 while($row = mysqli_fetch_array($result)) {
 	print '<div class="note" id='.$row['id'].'>';
-	print '<span>'.$row['doneOrNot'].'</span><h3>'.$row['title'].'</h3><br><h4>'.$row['datetime'].'</h4><p>'.$row['taskdescription'].'</p>';
+	print '<span onclick="changeState(this)">'.$row['doneOrNot'].'</span><h3>'.$row['title'].'</h3><br><h4>'.$row['DATETIME'].'</h4><p>'.$row['taskdescription'].'</p>';
 	print '</div>';
 }
 
