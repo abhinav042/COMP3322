18301f136ea7d3092a7ce5687fd6db482a301fdd
Abhinav Goyal <abhinav.goyal@live.com>
2017-12-04 08:43:31 +0800

    update project

--- ChatterBox/public/javascripts/myscripts.js
+++ ChatterBox/public/javascripts/myscripts.js
@@ -4,21 +4,19 @@ chatter_box.controller('chatController', function($scope, $http) {
     
     $scope.load = function() {
         $http.get("/load").then(res => {
-            if (res.data === "") {
+            console.log(res.data);
+            if (res.data == "") {
                 $scope.showLogin = true;
             }
             else {
-                console.log(`${res.data.name}`);
                 $scope.name = res.data.name;
                 $scope.friends = res.data.friends;
                 $scope.showLogin = false;
-                //$scope.name = res.data.name;
             }
         });
-    }
+    };
 
     $scope.login = function() {
-        console.log(`the username is : ${$scope.username}, the password is ${$scope.password}`);
         if ($scope.username && $scope.password) {
             $http.post("/login", {username:$scope.username, password:$scope.password}).then(res => {
                 if (res.data.msg === 'LOGIN INVALID') {
@@ -31,14 +29,22 @@ chatter_box.controller('chatController', function($scope, $http) {
         } else {
             alert("You must enter a username and password!");
         }
-    }
+    };
 
     $scope.logout = function() {
         $http.get("/logout").then(res => {
-            if (res.data === "") {
+            if (res.data == "") {
                 $scope.showLogin = true;
             }
         });
-    }
+    };
+
+    $scope.getConversation = function(friend_id) {
+        $http.get(`/getconversation/${friend_id}`).then(res => {
+            console.log(res.data);
+            $scope.showChat = true;
+            $scope.current_friend = res.data;
+        });
+    };
 
 });
--- ChatterBox/routes/chat.js
+++ ChatterBox/routes/chat.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const monk = require('monk');
+
 let sess;
 
 /* GET load */
 router.get('/load', (req, res, next) => {
   let responseJson = {};
   sess = req.session;
-  console.log(`the session id is ${req.session.userId}`);
+  // console.log(`the session id is ${req.session.userId}`);
   if (!sess.userId) {
     res.send("");
   } else {
@@ -18,19 +20,40 @@ router.get('/load', (req, res, next) => {
       const filter = {_id : sess.userId};
       const user = await collection.findOne(filter);
       const friend_promise = user.friends.map(friend => {
-        return collection.findOne({name:friend.name});
+        return collection.findOne({name:friend.name}, {fields: {name:1}});
       });
       const friend_data = await Promise.all([...friend_promise]);
-      
+      // console.log(friend_data);
+      const messageCollection = db.get('messageList');
+      const message_promise = friend_data.map(friend => {
+        let friend_id = friend._id.toString();
+        return messageCollection.find({senderId: friend_id, receiverId:req.session.userId});
+      });
+      const message_list = await Promise.all([...message_promise]);
+      // console.log(message_list);
+      message_list.forEach((message_collection, index) => {
+        let unread_messages = 0;
+        friend_data[index].unread_counter = 0;
+        for (let i = 0; i < message_collection.length; ++i) {
+          if (message_collection[i] == friend_data[index].lastMsgId) {
+            friend_data[index].unread_counter = unread_messages;
+            break;
+          } else if (i==message_collection.length-1){
+            friend_data[index].unread_counter = unread_messages + 1;
+            break;
+          }
+          unread_messages++;
+        }
+      });
       responseJson = {
         name: user.name,
         icon: user.icon,
         friends: friend_data
-      }
-      console.log(responseJson);
+      };
+      // console.log(responseJson);
+      res.json(responseJson);
     }
     returnLoad();
-    res.json(responseJson);
   }
 });
 
@@ -49,14 +72,14 @@ router.post('/login', (req, res, next) => {
         });
       } else {
         req.session.userId = docs[0]._id;
-        console.log(`The userId is ${req.session.userId}`);
+        // console.log(`The userId is ${req.session.userId}`);
         collection.update({_id: req.session.userId}, {$set: {status: 'online'}});
         const responseJSON = {
           'name' : docs[0].name,
           'icon' : docs[0].icon,
           'friends' : docs[0].friends
         }
-        res.send(responseJSON);
+        res.json(responseJSON);
       }
     }
   });
@@ -101,34 +124,28 @@ router.put('/saveuserinfo', (req, res, next) => {
 /* GET get conversation with friend */
 // TODO
 router.get('/getconversation/:friendid', (req, res, next) => {
-  let friend;
-  const friendId = req.params.friendid;
-  const db = req.db;
-  const collection = db.get('userList');
-  let icon;
-  let status;
-  let messageList;
-  let responseJSON;
-  collection.find({_id:friendId}, (err, docs) => {
-    let friend_name;
-    if (err === null) {
-      friend_name = docs.name;
-      icon = docs.icon;
-      status = docs.status;
-      const messageCollection = db.get('messageList');
-      messageCollection.find({senderId:req.session.userId, receiverId:friendId}, (err, docs) => {
-        if (err === null) messageList = docs;
-      });
-      messageCollection.find({senderId:friendId, receiverId:req.session.userId}, (err, docs) => {
-        if (err === null) messageList += docs;
-      });
-      // TODO update lastMsgId
-      collection.update({_id:"5a1f333627f2c2cb4bd454b8", "friends.name": friend_name}, {$set: {"friends.$.lastMsgId": 25}});
-    }
-  });
-  // TODO update lastMsgId
-  // collection.update({_id:"5a1f333627f2c2cb4bd454b8", "friends.name": friend_name}, {$set: {"friends.$.lastMsgId": 25}});
-  res.send(responseJSON);
+
+  async function loadConversation() {
+    const friendId = req.params.friendid;
+    const db = req.db;
+    const collection = db.get('userList');
+    const messageCollection = db.get('messageList');
+    const id = monk.id(friendId);
+    const friend = await collection.findOne({_id:id}, {fields: {name: 1, icon: 1, status: 1}});
+    // console.log(`the friend is ${friend}`);
+    // const user_id = (req.session.userId).toString();
+    const messageSentByFriend = await messageCollection.find({senderId: friendId, receiverId:'5a22e40c7ff807811558c2de'});
+    // console.log(messageSentByFriend);
+    const messageReceivedByFriend = await messageCollection.find({senderId:'5a22e40c7ff807811558c2de', receiverId: friendId});
+    // console.log(messageSentByFriend[0]._id.toString());
+    collection.update({_id:"5a22e40c7ff807811558c2de", "friends.name": friend.name}, {$set: {"friends.$.lastMsgId": messageSentByFriend[0]._id.toString()}});
+    friend.messageSentByFriend = messageSentByFriend;
+    friend.messageReceivedByFriend = messageReceivedByFriend;
+    // console.log(friend);
+    res.json(friend);
+  }
+  loadConversation();
+
 });
 
 /* POST send message to friend */
